301a67120702e2aed8877300f9fdf2e5df61a551
Aryan6919 <113785954+Aryan6919@users.noreply.github.com>
2023-01-15 03:58:59 +0530

    Add files via upload

---
+++ public/index.js
@@ -0,0 +1,7 @@
+function op(){
+    document.body.style.backgroundColor="#301934";
+    //  document.getElementById('textarea').style.backgroundColor="#00008B";
+    document.querySelector('.message__area').style.backgroundColor="#00008B";
+    document.getElementById('iop').innerHTML="Dark Mode Fired🔥"
+
+}
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+io.on('connection',(socket)=>
+// {
+//     socket.on('new-user joined',(name)=>{
+// users[socket.id]=name
+// socket.broadcast.emit('user-connected',name)
+//     }
+//     )
+// })
